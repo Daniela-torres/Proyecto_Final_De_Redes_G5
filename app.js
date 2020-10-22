454851ccae066f47c4b2aeec0c734a3a16d1682f
Yerardin-Paternina <ypaterninaacosta32@correo.unicordoba.edu.co>
2020-10-21 19:05:37 -0500

    Cambio saludo bienvenida
    se cambió el saludo de bienvenida

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express(); 
 const port = 3000; 
 app.get('/', (req, res) => { 
-  res.send('BIENVENIDOS SEAN TODOS'); 
+  res.send('Hola a todos'); 
 }) 
 
 app.listen(port, () => { 
